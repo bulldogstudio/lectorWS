725a7616e6dafb6259c1954494d793bdff6163d5
bulldogstudio <carlosgbcn@gmail.com>
2019-05-07 22:14:05 +0200

    Test
    Change-Id: I9bee7921c72c7b62f3948972123e4392fa0695a1
    Signed-off-by: bulldogstudio <carlosgbcn@gmail.com>

---
+++ src/main/java/net/fiol/demo/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package net.fiol.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
+}
